11cebeb6887ffc42e759b3f36261f196ba02732c
dimax0505 <dimax0505@gmail.com>
2018-12-06 15:36:31 +0300

    Подготовил код из методички для 5 ДЗ

--- src/com/maximov/java3/Lesson1/Main.java
+++ src/com/maximov/java3/Lesson1/Main.java
@@ -1,4 +1,32 @@
 package com.maximov.java3.Lesson1;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
 public class Main {
+    public static final int CARS_COUNT = 4;
+    public static void main(String[] args) {
+
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        }
+        CyclicBarrier cb = new CyclicBarrier(CARS_COUNT);
+        ExecutorService executorService = Executors.newCachedThreadPool(new ThreadFactoryBuilder()
+                .setNameFormat("Car - %s")
+                .build());
+
+
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start(cb);
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
 }
---
+++ src/com/maximov/java3/Lesson1/Race.java
@@ -0,0 +1,12 @@
+package com.maximov.java3.Lesson1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Race {
+    private ArrayList<Stage> stages;
+    public ArrayList<Stage> getStages() { return stages; }
+    public Race(Stage... stages) {
+        this.stages = new ArrayList<>(Arrays.asList(stages));
+    }
+}
